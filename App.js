7fc1a46dac9a43881b7a75405369eb638002146a
Bruno10log <bmeira.bms@gmail.com>
2020-07-13 21:10:00 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Home from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+import Contato from './src/pages/Contato';
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const icons = {
+  Home: {
+    name: 'ios-home'
+  },
+  Sobre: {
+    name: 'ios-people'
+  },
+  Contato: {
+    name: 'ios-call'
+  }
+}
+
+export function Tabs() {
+
+  return (
+      <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          const { name } = icons[route.name];
+          return <Icon name={name} color={color} size={size}></Icon>
+        }
+      })}
+        tabBarOptions={{
+          style: {
+            backgroundColor: '#121212'
+          },
+          activeTintColor: '#fff'
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Sobre" component={Sobre} /> 
+      </Tab.Navigator>
+  );
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Tabs} options={{ headerShown: false }} />
+        <Stack.Screen name="Contato" component={Contato} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
